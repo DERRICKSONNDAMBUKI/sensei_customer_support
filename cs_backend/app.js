b289b06c4ce52eeb8259819a9f7922c694aa7db9
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-13 13:00:15 +0300

    cs_server setup commit

--- cs_backend/app.js
+++ cs_backend/app.js
@@ -1,18 +1,23 @@
 const express = require("express");
-
-app = express()
-const cors = require('cors');
+app = express();
+const cors = require("cors");
 
 // middlewares
-app.use(express.json())
-app.use(cors)
+app.use(express.json());
+app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('<h1>customer support</h1>')
+// ROUTES
+app.get('/', async (req, res) => {
+    try {
+        res.send("Hello RU nav sys")
+        console.log('getting home');
+    } catch (error) {
+        console.error(error);
+    }
 })
 
-const server = app.listen(process.env.PORT||5000, "0.0.0.0", () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`server is listening at http://${host}:${port}`);
-  });
+const server = app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`server is listening at http://${host}:${port}`);
+});
