418725b20971bf709a832d17a2aadfcbdd1d05ef
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-13 13:20:54 +0300

    cs_server setup commit

--- cs_backend/app.js
+++ cs_backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 app = express();
 const cors = require("cors");
+require('dotenv').config()
 
 // middlewares
 app.use(express.json());
@@ -11,12 +12,13 @@ app.get('/', async (req, res) => {
     try {
         res.send("Hello RU nav sys")
         console.log('getting home');
+        console.log(process.env);
     } catch (error) {
         console.error(error);
     }
 })
 
-const server = app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
+const server = app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`server is listening at http://${host}:${port}`);
